f0bb62d74371948c7165508a81296af23df6e4b7
Luis Ch <Luisrachm8@gmail.com>
2024-09-12 00:05:37 -0600

    Mysql and user entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { CatsController } from './cats/cats.controller';
 import { CatsModule } from './cats/cats.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule, UsersModule, TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    //password: 'root',
+    database: 'superflights',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    autoLoadEntities: true,
+    synchronize: true
+  })],
   controllers: [, CatsController],
   providers: [],
 })
