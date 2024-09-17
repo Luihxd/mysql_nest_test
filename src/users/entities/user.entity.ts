892a8cfb9a97464d02d65025192cf89ba27d8b36
Luis Ch <Luisrachm8@gmail.com>
2024-09-16 23:24:26 -0600

    User find all working

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { Column, DeleteDateColumn, Entity } from "typeorm";
+
+@Entity()
+export class User {
+	@Column( { primary: true, generated: 'increment' } )
+	id: number;
+
+	@Column()
+	name: string;
+
+	@Column()
+	email: string;
+
+	@Column()
+	isActive: boolean;
+
+	@Column()
+	password: string;
+
+	//@DeleteDateColumn()
+	//deletedAt: Date;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  constructor(
+    @InjectRepository(User) 
+    private readonly usersRepository: Repository<User>,
+  ) {}
+  async create(createUserDto: CreateUserDto) {
+    return await this.usersRepository.save(createUserDto);
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    return await this.usersRepository.find();
   }
 
   findOne(id: number) {
